82bef240d1d5cafae48208b7cb77e789e267901e
Kervi Falcón <isaifalcon10m@gmail.com>
2025-05-12 16:41:55 -0500

    Paginacion

--- js/script2.js
+++ js/script2.js
@@ -1,9 +1,14 @@
 const detallesSegPan = document.getElementById('t');
 const ticketsPantalla2 = document.getElementById('ticket-body-pantalla2');
 const formBuscarPantalla2 = document.getElementById('form-buscar-pantalla2');
+var paginaTicket = 1;
+const resultadosPorPagina = 10;
+
 
 formBuscarPantalla2.addEventListener('submit', async (e) => {
     e.preventDefault();
+
+    paginaTicket = 1;
     const formData = new FormData(formBuscarPantalla2);
     const formDataJson = Object.fromEntries(formData.entries());
     obtenerDatosTicket(formDataJson)
@@ -11,7 +16,7 @@ formBuscarPantalla2.addEventListener('submit', async (e) => {
 
 
 async function obtenerDatosTicket(formData) {
-    const URL = "php/ObtenerTickets.php?contenido=" + formData.contenido;
+    const URL = "php/ObtenerTickets.php?contenido=" + formData.contenido + "&pagina=" + paginaTicket + "&resultadosPorPagina=" + resultadosPorPagina;
     const modalCargando = document.querySelector('.contenedor-cargando');
     try {
         modalCargando.style.display = "flex";
@@ -24,7 +29,7 @@ async function obtenerDatosTicket(formData) {
 
         const data = await response.json();
         mostrarTicket(data)
-
+        paginacion(data.totalResultados)
         modalCargando.style.display = "none";
     } catch (error) {
         console.log(error)
@@ -34,15 +39,173 @@ async function obtenerDatosTicket(formData) {
 }
 
 function mostrarTicket(datos) {
- const tickets = datos.datos;
- 
- if (tickets.length === 0) {
-    ticketsPantalla2
-    .innerHTML = "";
- }
+    const tick = datos['datos'];
+
+    if (tick.length === 0) {
+        ticketsPantalla2
+            .innerHTML = "";
+    } else {
+        ticketsPantalla2.innerHTML = "";
+        tick.forEach(e => {
+            const tr = document.createElement('tr');
+            const td1 = document.createElement('td');
+            const td2 = document.createElement('td');
+            const td3 = document.createElement('td');
+            const td4 = document.createElement('td');
+            const td5 = document.createElement('td');
+            const td6 = document.createElement('td');
+            const td7 = document.createElement('td');
+
+            td1.textContent = e.id_ticket;
+            td2.textContent = e.usuario_solicito;
+            td3.textContent = e.usuario_atendio ? e.usuario_atendio : 'Ninguno';
+            td4.textContent = e.progreso;
+            td5.textContent = e.tipo_ticket;
+            td6.textContent = prioridad(e.prioridad);
+            td7.classList.add('icon-eye');
+            const img = document.createElement('img');
+            img.src = 'img/icon-ojo.jpg';
+            img.width = 24;
+            img.height = 24;
+            img.addEventListener('click', () => {
+                verDetalles(e.id_ticket);
+            })
+            td7.appendChild(img);
+
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            tr.appendChild(td5);
+            tr.appendChild(td6);
+            tr.appendChild(td7);
+            ticketsPantalla2.appendChild(tr);
+        });
+
+    }
 
 }
 
 function saludar() {
     console.log("Hola")
+}
+
+function verDetalles(id) {
+    console.log(id)
+}
+
+function prioridad(prioridad) {
+    switch (prioridad) {
+        case 1:
+            return "Baja";
+            break;
+        case 2:
+            return "Media";
+            break;
+        case 3:
+            return "Alta";
+            break;
+        case 4:
+            return "Urgente";
+            break;
+    }
+}
+
+function paginacion(filas) {
+    const paginacionHtml = document.querySelector('.paginacion1');
+
+    paginacionHtml.innerHTML = "";
+    const totalPaginas = Math.ceil(filas / resultadosPorPagina);
+    console.log(totalPaginas)
+    const ul = document.createElement('ul');
+
+    if (totalPaginas === 0) {
+        paginacionHtml.innerHTML = "";
+    } else if (totalPaginas < 4) {
+
+        for (let i = 0; i < totalPaginas; i++) {
+            const li = document.createElement('li');
+            li.textContent = i + 1;
+            if (paginaTicket === i + 1) {
+                li.classList.add('enter');
+            } else {
+                li.addEventListener('click', () => {
+                    paginaTicket = i + 1;
+                    const formData = new FormData(formBuscarPantalla2);
+                    SetTimeout(() => {
+                        obtenerDatosTicket(formData)
+                    }, 500)
+                })
+            }
+            ul.appendChild(li);
+        }
+
+    } else {
+        for (let i = 0; i < totalPaginas; i++) {
+
+            const li = document.createElement('li');
+
+            if ((paginaTicket + i - 1) > 0 && (paginaTicket + i - 1) <= totalPaginas) {
+                li.textContent = (paginaTicket + i - 1);
+                if (paginaTicket === paginaTicket + i - 1) {
+                    li.classList.add('enter');
+                } else {
+                    li.addEventListener('click', () => {
+                        paginaTicket = paginaTicket + i - 1;
+                        console.log(paginaTicket)
+                        setTimeout(() => {
+                            const formData = new FormData(formBuscarPantalla2);
+                            obtenerDatosTicket(formData)
+                        }, 500)
+                    })
+                }
+                ul.appendChild(li);
+            }
+
+        }
+
+    }
+
+    if ((paginaTicket - 2) > 0) {
+        const li = document.createElement('li');
+        li.textContent = "«";
+        li.addEventListener('click', () => {
+            paginaTicket = 1;
+            formBuscarPantalla2.querySelector('input[type="submit"]').click();
+        })
+        const li2 = document.createElement('li');
+        li2.textContent = "...";
+        ul.insertBefore(li2, ul.firstChild);
+        ul.insertBefore(li, ul.firstChild);
+    }
+
+    if ((paginaTicket + 2) < totalPaginas) {
+        const li = document.createElement('li');
+        li.textContent = "»";
+        li.addEventListener('click', () => {
+            paginaTicket = totalPaginas;
+            /*             formBuscarPantalla2.querySelector('input[type="submit"]').click(); */
+            const formData = new FormData(formBuscarPantalla2);
+            SetTimeout(() => {
+                obtenerDatosTicket(formData)
+            }, 500)
+
+        })
+        const li2 = document.createElement('li');
+        li2.textContent = "...";
+        ul.appendChild(li2);
+        ul.appendChild(li);
+    }
+
+    paginacionHtml.appendChild(ul);
+
+
+
+    /*     <ul>
+        <li> « </li>
+        <li> 1</li>
+        <li> 2</li>
+        <li> 3 </li>
+        <li>»</li>
+    </ul> */
 }
--- php/ObtenerTickets.php
+++ php/ObtenerTickets.php
@@ -4,12 +4,17 @@ require('conexion.php');
 
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
   $contenido = $_GET['contenido'];
+  $pagina = $_GET['pagina'];
+  $resultadosPorPagina = $_GET['resultadosPorPagina'];
   $contenido = '%' . strtolower($contenido) . '%';
+
+  $filaInicial = ($pagina -1 ) * $resultadosPorPagina;
 
 $sql = 'SELECT t.*, u1.nombre as usuario_solicito, u2.nombre as usuario_atendio FROM tickets t
 LEFT JOIN usuarios u1 ON t.id_usuario_solicito = u1.id_usuario
 LEFT JOIN usuarios u2 ON t.id_usuario_atendio = u2.id_usuario
 WHERE LOWER(t.titulo) LIKE ? OR LOWER(t.descripcion) LIKE ? OR LOWER(t.tipo_ticket) LIKE ?
+LIMIT ? OFFSET  ?
 ';
 
 $stmt = $conn->prepare($sql);
@@ -20,8 +25,7 @@ if(!$stmt) {
     exit();
 }
 
-
-$stmt->bind_param("sss", $contenido, $contenido, $contenido);
+$stmt->bind_param("sssii", $contenido, $contenido, $contenido, $resultadosPorPagina, $filaInicial);
 
 $stmt->execute();
 $result = $stmt->get_result();
@@ -33,6 +37,25 @@ while($row = $result->fetch_assoc()) {
 }
 
 
-echo json_encode(["status"=>"success", "datos"=>$datos]);
+
+$sql = 'SELECT t.*, u1.nombre as usuario_solicito, u2.nombre as usuario_atendio FROM tickets t
+LEFT JOIN usuarios u1 ON t.id_usuario_solicito = u1.id_usuario
+LEFT JOIN usuarios u2 ON t.id_usuario_atendio = u2.id_usuario
+WHERE LOWER(t.titulo) LIKE ? OR LOWER(t.descripcion) LIKE ? OR LOWER(t.tipo_ticket) LIKE ?
+';
+$stmt = $conn->prepare($sql);
+if(!$stmt) {
+    http_response_code(500);
+    echo json_encode(["status"=>"error", "message" => "Error al generar la consulta"]);
+    exit();
+}
+$stmt->bind_param("sss", $contenido, $contenido, $contenido);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$totalResultados = $result->num_rows;
+
+
+echo json_encode(["status"=>"success", "datos"=>$datos, "totalResultados"=>$totalResultados]);
 
 }
